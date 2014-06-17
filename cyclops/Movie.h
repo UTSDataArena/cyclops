f15b8c51507d2f703386068fc29649d07a97d115
Darren Lee <darren.lee@uts.edu.au>
2014-06-17 11:12:06 +1000

    adding initial Movie playback capability for Cyclops using ffmpeg plugin from OpenSceneGraph [Darren 17Jun14]

---
+++ cyclops/Movie.h
@@ -0,0 +1,80 @@
+/**
+Data Arena Movie object
+
+[Darren 10Jun14]
+
+*/
+
+#include "cyclops/Entity.h"
+#include "cyclops/SceneManager.h"
+#include <osgDB/ReadFile>
+#include <osg/ImageStream>
+
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/StateSet>
+#include <osg/Texture2D>
+#include <osg/TextureRectangle>
+#include <osg/PositionAttitudeTransform>
+
+namespace cyclops {
+	using namespace omega;
+	using namespace omegaOsg;
+
+	class CY_API Movie: public Entity {
+	public:
+		
+		static Movie* create(const String& filePath, float width, float height);
+		
+		Movie(SceneManager* scene, const String& filePath="", float width=1.0f, float height=1.0f);
+		
+		virtual void rewind();
+		virtual void play();
+		virtual void pause();
+		
+		virtual void setLooping(bool loop);
+		
+		virtual double getCurrentTime();
+		virtual void seek(double time);
+		
+		virtual int getMovieWidth();
+		virtual int getMovieHeight();
+		
+		virtual void setMaskEnabled(bool enabled);
+		virtual void setMask(osg::Image* image);
+		virtual void setMaskFromFile(const String& filePath);
+		
+		virtual bool isPlaying();
+		
+		virtual osg::Geometry* myCreateTexturedQuadGeometry(
+			const osg::Vec3& pos,
+			float width,
+			float height, 
+			osg::Image* image, 
+			bool xyPlane, 
+			bool option_flip
+		);
+		
+		virtual float getAspectRatio() { return _aspectRatio; };
+		
+	protected:
+		
+		
+	private:
+		
+		float _aspectRatio;
+		
+		float myWidth;
+		float myHeight;
+		String myFilePath;
+		osg::ref_ptr<osg::ImageStream> imagestream;
+		
+		osg::Image* imageMask;
+		bool maskEnabled;
+		
+		osg::Texture2D* texture_mask;
+		
+		osg::StateSet* movieSet;
+	};
+
+}
---
+++ src/Movie.cpp
@@ -0,0 +1,270 @@
+/**
+Data Arena Movie object
+
+[Darren 10Jun14]
+
+*/
+
+#include "cyclops/Movie.h"
+
+using namespace cyclops;
+
+///////////////////////////////////////////////////////////////////////////////
+osg::Geometry* Movie::myCreateTexturedQuadGeometry(const osg::Vec3& pos,float width,float height, osg::Image* image, bool xyPlane, bool option_flip)
+{
+	bool flip = image->getOrigin()==osg::Image::TOP_LEFT;
+	if (option_flip) flip = !flip;
+
+	osg::ref_ptr<osg::Geometry> pictureQuad = osg::createTexturedQuadGeometry(pos,
+		osg::Vec3(width,0.0f,0.0f),
+		xyPlane ? osg::Vec3(0.0f,height,0.0f) : osg::Vec3(0.0f,0.0f,height),
+		0.0f, flip ? 1.0f : 0.0f , 1.0f, flip ? 0.0f : 1.0f);
+
+	osg::Texture2D* texture = new osg::Texture2D();
+	texture->setFilter(osg::Texture::MIN_FILTER,osg::Texture::LINEAR);
+	texture->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
+	texture->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
+	texture->setResizeNonPowerOfTwoHint(false);
+
+	texture->setImage(image);
+	
+	movieSet = pictureQuad->getOrCreateStateSet();
+	movieSet->setTextureAttributeAndModes(0,
+		texture,
+		osg::StateAttribute::ON);
+
+	texture_mask = new osg::Texture2D();
+	texture_mask->setImage(imageMask);
+	texture_mask->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
+	texture_mask->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
+	texture_mask->setResizeNonPowerOfTwoHint(false);
+	movieSet->setTextureAttributeAndModes(1,
+		texture_mask);
+	
+	
+// 		movieSet->setTextureAttributeAndModes(0,
+// 			texture);
+	
+
+	return pictureQuad.release();
+}
+
+
+Movie* Movie::create(const String& filePath, float width, float height) {
+	return new Movie(SceneManager::instance(), filePath, width, height);
+}
+
+Movie::Movie(SceneManager* scene, const String& filePath, float width, float height):
+	Entity(scene),
+	myWidth(width),
+	myHeight(height),
+	myFilePath(filePath) {
+
+	imageMask = NULL;
+	maskEnabled = false;
+		
+	static bool ffmpegPluginLoaded = false;		
+		
+	if (!ffmpegPluginLoaded) {
+		
+		// force ffmpeg plugin
+		std::string libName = osgDB::Registry::instance()->createLibraryNameForExtension("ffmpeg");
+		osgDB::Registry::instance()->loadLibrary(libName);
+		ffmpegPluginLoaded = true;
+	}
+		
+		
+	// The node containing the scene
+	osg::ref_ptr<osg::Node> node = new osg::Node();
+	
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+
+	// if no model name found:
+	if(filePath == "")
+	{
+		
+		owarn("No movie specified!!");
+		
+		return;
+
+	}
+	
+	osg::StateSet* stateset = geode->getOrCreateStateSet();
+	// stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+	
+	// shader to use
+	static const char *shaderSourceTexture2D = {
+		"uniform sampler2D movie_texture;\n"
+		"uniform sampler2D mask_texture;\n"
+		"uniform bool mask_enabled;\n"
+		"void main(void)\n"
+		"{\n"
+		"    vec4 texture_color = texture2D(movie_texture, gl_TexCoord[0].st); \n"
+		"    float alpha = texture2D(mask_texture, gl_TexCoord[0].st);\n"
+		"    gl_FragColor = texture_color;\n"
+		"    gl_FragColor[3] = mask_enabled ? alpha : 1;\n"
+		"}\n"
+	};
+
+	osg::Program* program = new osg::Program;
+
+	program->addShader(new osg::Shader(osg::Shader::FRAGMENT, shaderSourceTexture2D));
+
+	stateset->addUniform(new osg::Uniform("movie_texture",0));
+	stateset->addUniform(new osg::Uniform("mask_texture", 1));
+
+	stateset->setAttribute(program);
+
+	// quad dimensions
+	osg::Vec3 pos(0.0f,0.0f,0.0f);
+	osg::Vec3 topleft = pos;
+	osg::Vec3 bottomright = pos;
+
+	imagestream = NULL;
+		
+	String path;
+	
+	if(DataManager::findFile(filePath, path)) {
+		
+		ofmsg("reading %1%", %path);
+		osg::ref_ptr<osg::Image> image = osgDB::readImageFile(path);
+		imagestream = dynamic_cast<osg::ImageStream*>(image.get());
+		
+		if (imagestream) {
+			ofmsg("i have an image stream with %1%", %imagestream->libraryName());
+			ofmsg("i have %1% frames", %(imagestream->getLength() * imagestream->getFrameRate()));
+			
+			imagestream->setLoopingMode(osg::ImageStream::LOOPING);
+			imagestream->play();
+			
+			ofmsg("stream status is %1%", %imagestream->getStatus());
+			
+			float width = imagestream->s() * imagestream->getPixelAspectRatio();
+			float height = imagestream->t();
+			
+			_aspectRatio = width / height;
+			
+			osg::Drawable* drawable = myCreateTexturedQuadGeometry(pos, myWidth, myHeight, imagestream.get(), true, false);			
+			geode->addDrawable(drawable);
+			
+			bottomright = pos + osg::Vec3(width, height, 0.0f);
+			// not in xy plane
+			pos.z() += height * 1.05f;
+			
+		} else {
+			ofwarn("!Failed to load movie: %1% (unsupported file format or corrupted data)", %path);
+			return;
+		}
+		
+		float r = geode->getBound().radius() * 2;
+		float scale = 1 / r;
+		
+		osg::PositionAttitudeTransform* pat = new osg::PositionAttitudeTransform();
+		
+		pat->setPosition(osg::Vec3(-(myWidth / 2.0), -(myHeight / 2.0), 0));
+		
+		pat->setScale(osg::Vec3(scale, scale, scale));
+		pat->addChild(geode);
+// 		node = pat;
+		node = geode;
+		
+		setCullingActive(false);
+		initialize(node);
+
+		getMaterial()->setColor(Color(1,1,1), Color(0,0,0));
+		getMaterial()->setTransparent(true);
+	}
+	else
+	{
+		ofwarn("!File not found: %1%", %filePath);
+		return;
+	}
+
+	
+	
+}
+
+inline int Movie::getMovieWidth() { if (imagestream) { return imagestream->s(); } else { return 0; } }
+
+inline int Movie::getMovieHeight() { if (imagestream) { return imagestream->t(); } else { return 0; } }
+
+
+void Movie::rewind() {
+	if (!imagestream) return;
+	
+	imagestream->rewind();
+	imagestream->play();
+	
+}
+
+void Movie::play() {
+	if (!imagestream) return;
+	
+	osg::ImageStream::StreamStatus playToggle = imagestream->getStatus();
+	if (playToggle != osg::ImageStream::PLAYING)
+	{
+		std::cout<< imagestream.get() << " Play"<< std::endl;
+		imagestream->play();
+	}
+	
+}
+
+void Movie::pause() {
+	if (!imagestream) return;
+	osg::ImageStream::StreamStatus playToggle = imagestream->getStatus();
+	if (playToggle == osg::ImageStream::PLAYING)
+	{
+		std::cout<< imagestream.get() << " Pause"<< std::endl;
+		imagestream->pause();
+	}
+	
+}
+
+void Movie::setLooping(bool loop) {
+	if (!imagestream) return;
+	if (loop) {
+		imagestream->setLoopingMode(osg::ImageStream::LOOPING);
+	} else {
+		imagestream->setLoopingMode(osg::ImageStream::NO_LOOPING);
+	}
+}
+
+double Movie::getCurrentTime() {
+	if (!imagestream) return -1;
+	
+	return imagestream->getReferenceTime();
+}
+
+void Movie::seek(double time) {
+	if (!imagestream) return;
+	std::cout<< imagestream.get() << " Seek: "<< time <<std::endl;
+	ofmsg("stream status is %1%", %imagestream->getStatus());
+	imagestream->seek(time);
+}
+
+void Movie::setMaskEnabled(bool enabled) {
+	maskEnabled = enabled;
+}
+
+void Movie::setMask(osg::Image* image) {
+	imageMask = image;
+	
+	if (imageMask) {
+		texture_mask->setImage(imageMask);
+		movieSet->setTextureAttributeAndModes(1, texture_mask);
+		maskEnabled = true;
+	}
+}
+
+void Movie::setMaskFromFile(const String& filePath) {
+	setMask(osgDB::readImageFile(filePath));
+}
+
+bool Movie::isPlaying() {
+
+	if (imagestream) { 
+		return (imagestream->getStatus() == osg::ImageStream::PLAYING);
+	} 
+	
+	return false;
+}
